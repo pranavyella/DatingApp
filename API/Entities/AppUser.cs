1a4cc9dea2c2536a688a6aee22357fa412ea12fc
Pranav Yella <pranav.yella@gmail.com>
2023-09-30 20:41:34 -0400

    created database and user entities

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace API.Entities;
+
+public class AppUser
+{
+    // EF will recognize this as the primary key
+    // Can also use [Key] attribute
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
