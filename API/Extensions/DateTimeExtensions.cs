fee2ad894963cecb4013bf750f02a9c86a3e2891
Pranav Yella <pranav.yella@gmail.com>
2024-01-08 13:57:53 -0500

    added randomly generated seeded data for users

---
+++ API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace API;
+
+public static class DateTimeExtensions
+{
+    public static int CalculateAge(this DateOnly dob) {
+        var today = DateOnly.FromDateTime(DateTime.Today);
+        var age = today.Year - dob.Year;
+        if (dob.AddYears(age) > today) age--;
+        return age;
+    }
+}
